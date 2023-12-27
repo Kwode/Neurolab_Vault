57599e063ee60c143908c22ca5224a06a367153e
Kwode Great <154435066+Kwode@users.noreply.github.com>
2023-12-27 12:43:46 +0100

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java,sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Medicine extends Main{
 	
